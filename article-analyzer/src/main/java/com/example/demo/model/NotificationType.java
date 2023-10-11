6bb404c1985af035870bf743ffbdc0585327a449
Oleksandr Bushyn <Oleksandr_Bushyn@epam.com>
2023-10-11 17:10:30 +0300

    Task 3 updated code according comments in MR

--- article-analyzer/src/main/java/com/example/demo/model/NotificationType.java
+++ article-analyzer/src/main/java/com/example/demo/model/NotificationType.java
@@ -1,17 +1,5 @@
 package com.example.demo.model;
 
 public enum NotificationType {
-  EMAIL("emailNotificationService"),
-  SLACK("slackNotificationService");
-
-  private final String serviceBeanName;
-
-  NotificationType(String serviceBeanName) {
-    this.serviceBeanName = serviceBeanName;
-  }
-
-  public String getServiceBeanName() {
-    return serviceBeanName;
-  }
-
+  EMAIL,  SLACK
 }
