fffc309ac971c9859d4e90d34b6a94d112e04e57
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-10-13 17:38:56 +0300

    - Removed unused imports

--- article-analyzer/src/main/java/co/inventorsoft/academy/spring/Application.java
+++ article-analyzer/src/main/java/co/inventorsoft/academy/spring/Application.java
@@ -1,11 +1,9 @@
 package co.inventorsoft.academy.spring;
 
 import co.inventorsoft.academy.spring.service.ArticleAnalyzerService;
-import co.inventorsoft.academy.spring.service.ArticleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Application {
