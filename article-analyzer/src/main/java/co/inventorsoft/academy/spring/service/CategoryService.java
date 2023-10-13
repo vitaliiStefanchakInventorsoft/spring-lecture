819a518ad8badb9c367ef53dbf85e0030aba221e
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-10-13 18:53:49 +0300

    - Fixed getCategories() method

--- article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/CategoryService.java
+++ article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/CategoryService.java
@@ -4,27 +4,33 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     public Set<String> getCategories(List<List<String>> splitWords) {
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Set<String> result = new HashSet<>();
 
         for (List<String> sentence : splitWords) {
+            Map<String, Integer> wordCountMap = new HashMap<>();
+
             for (String word : sentence) {
                 wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
             }
+
+            int maxCount = Collections.max(wordCountMap.values());
+
+            for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+                if (entry.getValue() == maxCount) {
+                    result.add(entry.getKey());
+                }
+            }
         }
 
-        int maxCount = Collections.max(wordCountMap.values());
-
-        return wordCountMap.entrySet().stream()
-                .filter(entry -> entry.getValue() == maxCount)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toSet());
+        return result;
     }
 }
+
