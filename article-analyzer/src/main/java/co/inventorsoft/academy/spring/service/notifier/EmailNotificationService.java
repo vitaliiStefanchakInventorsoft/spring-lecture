394c9022ac36a100ab40e66683e4bf5521e24f7c
appsterlight <appsterlight@gmail.com>
2023-10-17 00:13:03 +0300

    add missed @Component annotation to EmailNotificationService and SlackNotificationService

--- article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/notifier/EmailNotificationService.java
+++ article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/notifier/EmailNotificationService.java
@@ -1,10 +1,12 @@
 package co.inventorsoft.academy.spring.service.notifier;
 
 import co.inventorsoft.academy.spring.model.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailNotificationService extends NotificationService {
     @Override
     public void notifyUsers(User user, String categories) {
-        System.out.printf(">>> Sending email ---> Header: Hello subscriber %s! Here is a list of categories | Content: %s%n", user.getEmail(), categories);
+        System.out.printf(">>> Sending email ---> Header: Hello subscriber %s! Here is a list of categories | Content: %s%n", user.getUsername(), categories);
     }
 }
--- article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/notifier/SlackNotificationService.java
+++ article-analyzer/src/main/java/co/inventorsoft/academy/spring/service/notifier/SlackNotificationService.java
@@ -1,10 +1,12 @@
 package co.inventorsoft.academy.spring.service.notifier;
 
 import co.inventorsoft.academy.spring.model.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SlackNotificationService extends NotificationService {
     @Override
     public void notifyUsers(User user, String categories) {
-        System.out.printf(">>> Sending Slack message ---> Header: Hello subscriber %s! Here is a list of categories | Content: %s%n", user.getSlackId(), categories);
+        System.out.printf(">>> Sending Slack message ---> Header: Hello subscriber %s! Here is a list of categories | Content: %s%n", user.getUsername(), categories);
     }
 }
