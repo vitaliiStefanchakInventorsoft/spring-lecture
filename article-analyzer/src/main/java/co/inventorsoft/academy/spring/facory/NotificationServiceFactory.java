9997198313ceea733000c1063087cf0935402e60
appsterlight <appsterlight@gmail.com>
2023-10-16 23:09:38 +0300

    Refactor to use constructor injection for EmailNotificationService and SlackNotificationService

--- article-analyzer/src/main/java/co/inventorsoft/academy/spring/facory/NotificationServiceFactory.java
+++ article-analyzer/src/main/java/co/inventorsoft/academy/spring/facory/NotificationServiceFactory.java
@@ -1,22 +1,29 @@
 package co.inventorsoft.academy.spring.facory;
 
+import co.inventorsoft.academy.spring.model.NotificationType;
+import co.inventorsoft.academy.spring.service.notifier.EmailNotificationService;
+import co.inventorsoft.academy.spring.service.notifier.NotificationService;
+import co.inventorsoft.academy.spring.service.notifier.SlackNotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 @Component
 public class NotificationServiceFactory {
+    private final EmailNotificationService emailNotificationService;
+
+    private final SlackNotificationService slackNotificationService;
 
     @Autowired
-    private EmailNotificationService emailNotificationService;
-
-    @Autowired
-    private SlackNotificationService slackNotificationService;
+    public NotificationServiceFactory(EmailNotificationService emailNotificationService, SlackNotificationService slackNotificationService) {
+        this.emailNotificationService = emailNotificationService;
+        this.slackNotificationService = slackNotificationService;
+    }
 
     public NotificationService getService(NotificationType type) {
-        switch (type) {
-            case EMAIL:
-                return emailNotificationService;
-            case SLACK:
-                return slackNotificationService;
-            default:
-                throw new IllegalArgumentException("Unknown notification type: " + type);
-        }
+        return switch (type) {
+            case EMAIL -> emailNotificationService;
+            case SLACK -> slackNotificationService;
+            default -> throw new IllegalArgumentException("Unknown notification type: " + type);
+        };
     }
 }
