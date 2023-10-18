d5780ce67ad618415de86986fcf8c8afd92e3647
KomaritsaKseniia06 <128077066+KomaritsaKseniia06@users.noreply.github.com>
2023-10-18 12:30:36 +0000

    Create new logic class Analyzer

--- article-analyzer/src/main/java/com/reader/article_analyzer/ArticleAnalyzerApplication.java
+++ article-analyzer/src/main/java/com/reader/article_analyzer/ArticleAnalyzerApplication.java
@@ -12,18 +12,8 @@ public class ArticleAnalyzerApplication {
 
     public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext context = SpringApplication.run(ArticleAnalyzerApplication.class, args);
-
-        ArticleCategoryAnalyzer analyzer = context.getBean(ArticleCategoryAnalyzer.class);
-        ArticleCategorySaver saver = context.getBean(ArticleCategorySaver.class);
-        UserNotifier userNotifier = context.getBean(UserNotifier.class);
-
-        analyzer.analyzeArticleContent("articles.json", 0);
-        analyzer.analyzeArticleContent("articles.json", 1);
-        analyzer.analyzeArticleContent("articles.json", 2);
-
-        saver.JsonSaver();
-
-        userNotifier.notifyUsersAboutCategoryChanges();
+        Analyzer analyzer = context.getBean(Analyzer.class);
+        analyzer.analyze();
 
     }
 }
