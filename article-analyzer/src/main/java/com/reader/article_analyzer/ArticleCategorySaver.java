d105fc13f4b6758f8bca43bef1e326cb300672b8
KomaritsaKseniia06 <128077066+KomaritsaKseniia06@users.noreply.github.com>
2023-10-18 14:19:41 +0000

    Rename method in ArticleCategorySaver

--- article-analyzer/src/main/java/com/reader/article_analyzer/ArticleCategorySaver.java
+++ article-analyzer/src/main/java/com/reader/article_analyzer/ArticleCategorySaver.java
@@ -14,7 +14,7 @@ public class ArticleCategorySaver {
         this.jsonFileService = jsonFileService;
     }
 
-    public void articlesSaver() throws IOException {
+    public void saveArticles() throws IOException {
         jsonFileService.writeJsonFile(articleCategoryAnalyzer.analyzeArticleContent("articles.json").stream().toList());
     }
 }
